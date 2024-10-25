8c3d9db03f8fbd74065877e3138edac27c85be3d
abhishek bora <abhishekbora244@gmail.com>
2024-10-25 08:16:34 +0530

    added a scheduler

--- UtilityBot.py
+++ UtilityBot.py
@@ -2,3 +2,16 @@ import discord
 from discord.ext import commands, tasks
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import requests
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import discord
+
+scheduler = AsyncIOScheduler()
+
+async def send_reminder(ctx, message):
+    await ctx.send(message)
+
+
+@scheduler.scheduled_job('interval', minutes=15)
+async def reminder_task():
+    await send_reminder(ctx, "Time to walk the dog!")
